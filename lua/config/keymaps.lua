e68affc1c73c8845779122683d2f109e83bd6ed1
Rodi-Janssen <rodijanssen@live.nl>
2025-10-04 14:45:22 +0200

    update config
    added background and plugins

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,28 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<C-h>", ":tabr<cr>", { desc = "open left tab" })
+vim.keymap.set("n", "<C-l>", ":tabl<cr>", { desc = "open right tab" })
+vim.keymap.set("n", "<C-n>", ":tabnew<cr>", { desc = "open new tab" })
+
+vim.keymap.set("n", "<C-s>", ":w<cr>", {})
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set("i", "<CapsLock>", "<Esc>", { expr = true, silent = true, desc = "esc" })
+vim.keymap.set("n", ";", ":", { desc = "remap" })
+
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
+vim.keymap.set("n", "<leader>cf", ":put =expand('%:p')<CR>", { desc = "current_file path" })
+
+-- Completely remove background from all UI elements
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
+vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
+vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "TabLine", { bg = "none" })
+vim.api.nvim_set_hl(0, "TabLineFill", { bg = "none" })
+vim.api.nvim_set_hl(0, "TabLineSel", { bg = "none" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,14 +1,5 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.keymap.set("n", "<C-h>", ":tabr<cr>", { desc = "open left tab" })
-vim.keymap.set("n", "<C-l>", ":tabl<cr>", { desc = "open right tab" })
-vim.keymap.set("n", "<C-n>", ":tabnew<cr>", { desc = "open new tab" })
-
-vim.keymap.set("n", "<C-s>", ":w<cr>", {})
-vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-vim.keymap.set("i", "<CapsLock>", "<Esc>", { expr = true, silent = true, desc = "esc" })
-vim.keymap.set("n", ";", ":", { desc = "remap" })
 
 vim.g.lazyvim_check_order = false
